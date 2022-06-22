2af9824d9a0faadab3c3a007805f980f643a2186
Sonia Pepe <sogispe@gmail.com>
2022-06-23 00:27:47 +0300

    Added error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import styles from './App.module.scss'
 import AutoComplete from './Components/AutoComplete/AutoComplete'
+import ErrorPage from './Components/ErrorPage/ErrorPage'
 import { useDebounceString } from './utils'
 import { useLazyGetCitiesByNameQuery } from './api/citiesApi'
 
 const App = () => {
-    const [trigger, { data, isFetching, isSuccess }] = useLazyGetCitiesByNameQuery()
+    const [trigger, { data, isFetching, isSuccess, isError, error }] = useLazyGetCitiesByNameQuery()
     const [currentInputValue, setCurrentInputValue] = useState('')
     const searchValue = useDebounceString(currentInputValue, 300)
     const minValueLength = 3
     const currentList = isSuccess && data ? data : []
 
     //TODO: Add background image to body and spinner until site is fully loaded
-    //console.log('isLoading', isLoading)
 
-    //TODO: Create Error Page
-    // console.log('isError', isError)
-    // console.log('error', error)
+    useEffect(() => {
+        if (searchValue.length >= minValueLength) {
+            trigger(searchValue)
+        }
+    }, [searchValue, trigger])
+
+    if (isError) {
+        return <ErrorPage />
+    }
 
     const onValueChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value
@@ -25,14 +31,8 @@ const App = () => {
 
     const onSelectedItemClick = (value: string) => setCurrentInputValue(value)
 
-    useEffect(() => {
-        if (searchValue.length >= minValueLength) {
-            trigger(searchValue)
-        }
-    }, [searchValue, trigger])
-
     return (
-        <div className={styles.app}>
+        <div onLoad={() => console.log('LOADED')} className={styles.app}>
             <div className={styles.wrapper}>
                 <h1>Welcome to the Cities Finder</h1>
                 <h2>Start typing and we'll help you find the city you're looking for!</h2>
